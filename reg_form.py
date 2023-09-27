afd4559e754a013017889f542bed39d0f6dab498
Aditya Aman <72183883+aditya-1967@users.noreply.github.com>
2023-09-27 11:45:53 +0530

    Updated

--- reg_form.py
+++ reg_form.py
@@ -5,25 +5,86 @@ import os
 import qrcode
 from PIL import Image
 
+def clear_form():
+    entry_name.delete(0, 'end')
+    entry_age.delete(0, 'end')
+    gender_var.set('Male')
+    text_medical_history.delete('1.0', 'end')
+    entry_aadhar.delete(0, 'end')
+
 def submit_form():
     name = entry_name.get()
     age = entry_age.get()
     gender = gender_var.get()
     medical_history = text_medical_history.get('1.0', 'end-1c')
+    aadhar_number = entry_aadhar.get()
 
-    if not name or not age or not gender:
+    if not name or not age or not gender or not aadhar_number:
         messagebox.showerror('Error', 'Please fill in all required fields')
         return
 
-    registration_data = {
-        "Name": name, 
-        "Age": age, 
-        "Gender": gender,
-        "Medical History": medical_history
-    }
+    # registration_data = {
+    #     "Name": name, 
+    #     "Age": age, 
+    #     "Gender": gender,
+    #     "Medical History": medical_history
+    # }
 
-    with open("registration.json", 'a') as file:
-        json.dump(registration_data, file, indent = 4)
+    # with open("registration.json", 'a') as file:
+    #     json.dump(registration_data, file, indent = 4)
+
+    # if os.path.exists('registration.json'):
+    #     with open('registration.json', 'r') as file:
+    #         data = json.load(file)
+    #         if aadhar_number in data:
+    #             data[aadhar_number].update({
+    #                 "Name": name, 
+    #                 "Age": age, 
+    #                 "Gender": gender,
+    #                 "Medical History": medical_history
+    #             })
+    #         else:
+    #             data[aadhar_number]({
+    #                 "Name": name, 
+    #                 "Age": age, 
+    #                 "Gender": gender,
+    #                 "Medical History": medical_history
+    #             })
+    # else:
+    #     data = {
+    #         aadhar_number: {
+    #             "Name": name, 
+    #             "Age": age, 
+    #             "Gender": gender,
+    #             "Medical History": medical_history
+    #         }}
+
+    data = {}
+
+    if os.path.exists('registration.json'):
+        with open('registration.json', 'r') as file:
+            try: 
+                data = json.load(file)
+            except json.JSONDecodeError:
+                data = {}
+
+    if aadhar_number in data:
+        data[aadhar_number].update({
+            "Name": name, 
+            "Age": age,
+            "Gender": gender,
+            "Medical History": medical_history
+        })
+    else:
+        data[aadhar_number] = {
+            "Name": name, 
+            "Age": age, 
+            "Gender": gender,
+            "Medical History": medical_history
+        }
+    
+    with open('registration.json', 'w') as file:
+        json.dump(data, file, indent=4)
 
     qr = qrcode.QRCode(
         version = 1,
@@ -32,7 +93,7 @@ def submit_form():
         border = 4,
     )
 
-    qr.add_data(json.dumps(registration_data))
+    qr.add_data(json.dumps(data[aadhar_number]))
     qr.make(fit = True)
     qr_img = qr.make_image(fill_color = "black", back_color = "white")
 
@@ -41,7 +102,7 @@ def submit_form():
     qr_img.save(os.path.join(patient_folder, "registration_qr.png"))
 
     messagebox.showinfo('Success', 'Registration Successful!')
-
+    clear_form()
 
 root = tk.Tk()
 root.title('Hospital Registration Form')
@@ -69,6 +130,11 @@ label_medical_history = tk.Label(root, text = "Medical History:")
 label_medical_history.pack()
 text_medical_history = tk.Text(root, height = 5, width = 30)
 text_medical_history.pack()
+
+label_aadhar = tk.Label(root, text='Aadhar Number:')
+label_aadhar.pack()
+entry_aadhar = tk.Entry(root)
+entry_aadhar.pack()
 
 submit_button = tk.Button(root, text = "Submit", command = submit_form)
 submit_button.pack()
